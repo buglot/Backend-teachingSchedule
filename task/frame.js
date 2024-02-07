ec595312c569845d18ea131fa5d5db0689e21f01
FrameFrame643 <werty.Rong@gmail.com>
2024-02-07 18:35:40 +0700

    frameopendoneeeiei

--- task/frame.js
+++ task/frame.js
@@ -35,26 +35,42 @@ router.get('/user',(req,res)=>{
     } else {
       res.status(401).json({ error: 'Invalid credentials' });
     }
-  });
-
-  router.post('/user1',(req,res)=>{
-    const email = req.body.email
-    const name = req.body.name
-    const role_id = req.body.id
-    const sql ='INSERT INTO user (email,name,role_id) VALUES (?,?,?)'
-    db.query(sql,[email,name,role_id],(err,result)=>{
-      if (err) {
-        return res.status(400).send("เกิดข้อผิดพลาดในการเพิ่มข้อมูล");
-      } else {
-        return res.status(200).send("ค่าถูกเพิ่มเข้าสู่ฐานข้อมูล");
-        
-      }
-    })
   })
 });
 
-//
+router.post('/user1',(req,res)=>{
+  const email = req.body.email
+  const name = req.body.name
+  const role_id = req.body.id
+  const sql ='INSERT INTO user (email,name,role_id) VALUES (?,?,?)'
+  db.query(sql,[email,name,role_id],(err,result)=>{
+    if (err) {
+      return res.status(400).send("เกิดข้อผิดพลาดในการเพิ่มข้อมูล");
+    } else {
+      return res.status(200).send("ค่าถูกเพิ่มเข้าสู่ฐานข้อมูล");
+        
+    }
+  })
+});
 
+//หน้ารายวิชาที่เปิดสอนdsadsadsa
+
+router.get('/opensubject',(req,res)=>{
+  const sql = 'select subjectsopen.id,idsubject,subject_category.name as category,subjects.name as subject_name,years,subjects.subject_category_id from subjects,subject_category,subjectsopen where subjectsopen.id = subjects.id and subject_category.id =  subjects_id'
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing SELECT statement:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    if (results.length > 0) {
+      res.json({ message: results });
+    } else {
+      res.status(401).json({ error: 'Invalid credentials' });
+    }
+  })
+});
 
 
 
