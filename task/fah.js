1d84e23c7c0dcd4a220c480d00edabfbadde9b09
Buglot <65818720+buglot@users.noreply.github.com>
2024-02-08 17:03:16 +0700

    at module upload file and log color

--- task/fah.js
+++ task/fah.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const multer = require('multer');
+const path = require('path');
 //ตัวอย่าง
-router.get('/login', (req, res) => {
-    const sql = 'SELECT * FROM table1';
-    db.query(sql, (err, results) => {
-      if (err) {
-        console.error('Error executing SELECT statement:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-  
-      if (results.length > 0) {
-        res.json({ message: results });
-      } else {
-        res.status(401).json({ error: 'Invalid credentials' });
-      }
-    });
-  });
-
 //เขียนเลย
 
 // READ user from db (database email ชื่อนามสกุล  role)
