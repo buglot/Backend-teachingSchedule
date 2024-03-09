eb38fd42fdbbc8735746f780f9f27eef54413779
Buglot <65818720+buglot@users.noreply.github.com>
2024-03-10 00:03:06 +0700

    updatefiles change name forler

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/api', fahRoutes);
 app.use('/api', settingRoutes);
 app.get('/download/:file', (req, res) => {
   const { file } = req.params;
-  res.sendFile(path.join(__dirname, 'public/files/' + file));
+  res.sendFile(path.join(__dirname, 'public/savefiles/' + file));
 })
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
